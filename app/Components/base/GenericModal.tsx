8f3adfe2df6ed6df00250378d24267da8943389e
Mark Nenadov <marknenadov@gmail.com>
2023-06-23 07:37:21 -0400

    modal width fix

--- app/Components/base/GenericModal.tsx
+++ app/Components/base/GenericModal.tsx
@@ -10,16 +10,13 @@ interface GenericModalProps {
 export default function GenericModal( {title, children, closeHandler}: GenericModalProps) {
     return (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="bg-white p-6 rounded shadow-md border border-1 border-black text-center">
-                <h2 className="text-xl font-semibold mb-4">{ title }</h2>
-                <div className="text-left">
-                    { children }
-                </div>
-                <GenericButton
-                    text="Close"
-                    onClick={ closeHandler }
-                />
+        <div className="bg-white p-6 rounded shadow-md border border-1 border-black text-center max-w-[90%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[50%]">
+            <h2 className="text-xl font-semibold mb-4">{title}</h2>
+            <div className="text-left">
+                {children}
             </div>
+            <GenericButton text="Close" onClick={closeHandler} />
         </div>
+    </div>
     );
 }
