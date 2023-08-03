a4589645c1ae0a334334a11dfde68924168113dc
Mark Nenadov <marknenadov@gmail.com>
2023-08-03 07:03:29 -0400

    set button attribute

--- app/Components/base/GenericButton.tsx
+++ app/Components/base/GenericButton.tsx
@@ -7,8 +7,9 @@ interface GenericButtonProps {
 export default function GenericButton( {text, shortText, onClick}: GenericButtonProps) {
     return (
         <button
+            type="button"
             className="bg-white hover:bg-gray-200 font-semibold py-2 px-4 rounded mt-4  border border-1 border-black"
-            onClick={onClick }
+            onClick={ onClick }
         >
             <span className="hidden lg:block">{ text }</span>
             <span className="block lg:hidden">{ !!shortText ? shortText : text }</span>
