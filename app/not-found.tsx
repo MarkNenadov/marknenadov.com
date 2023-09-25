ef913c10aebfe19117e5bb943c068230db8a08ba
Mark Nenadov <marknenadov@gmail.com>
2023-09-25 16:23:07 -0400

    implement custom 404

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SocialMediaLinks from './Components/SocialMediaLinks';
+
+export default function Custom404() {
+    return (
+        <div className="min-h-[100vh] max-w-7xl m-3">
+            <div className={"flex flex-col space-y-3 p-4  m-4 w-full items-center"}>
+                <div className="text-6xl text-center">Mark Nenadov</div>
+                <div>Ever curious, ever learning.</div>
+
+                <SocialMediaLinks />
+
+                <div className="w-full m-3 p-3 border border-1 border-black">
+                    <span className="text-5xl">404</span>
+                    <div>The page you are looking for does not exist. <a className="underline text-blue-900" href="/">Try this</a>.</div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
