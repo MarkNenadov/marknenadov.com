682ce54884f76aaf206d884d8db9eca213b86a4f
Mark Nenadov <marknenadov@gmail.com>
2023-12-16 20:36:44 -0500

    accessibility fix on generic modal

--- app/Components/base/GenericModal.tsx
+++ app/Components/base/GenericModal.tsx
@@ -9,11 +9,14 @@ interface GenericModalProps {
 
 export default function GenericModal( {title, children, closeHandler}: GenericModalProps) {
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div 
+            className="fixed inset-0 flex items-center justify-center z-50"
+            aria-labelledby={`modal-title-${title}`}
+            >
         <div 
             className="bg-white p-6 rounded shadow-md border border-1 border-black text-center max-w-[90%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[50%]"
         >
-            <h2 className="text-xl font-semibold mb-4">{title}</h2>
+            <h2 id={`modal-title-${title}`} className="text-xl font-semibold mb-4">{title}</h2>
             <div className="text-left">
                 {children}
             </div>
