ed449a473bbeddf13fc85105c66e2add1b9c1c4b
Mark Nenadov <marknenadov@gmail.com>
2024-05-25 08:14:56 -0400

    update bug year count

--- app/Components/AmateurNaturalistCard.tsx
+++ app/Components/AmateurNaturalistCard.tsx
@@ -13,7 +13,7 @@ export const AmateurNaturalistCard = () => {
                 I&apos;m more or less interested in every living creature, but over the years birds and moths have particularly caught my imagination. I earnestly collect data for various citizens cience projects, such as iNaturalist (I&apos;ve recently posted my 29,000th observation).
               </p> 
               <p className="mb-4">
-                In 2023, I documented 668 research-grade iNaturalist Arthropod species in Essex County, Ontario.  Check out the <a className="underline text-blue-900" href="https://www.cbc.ca/listen/live-radio/1-80-afternoon-drive/clip/15991630-a-bug-year">CBC radio interview</a> and the <a className="underline text-blue-900" href="https://inaturalist.ca/projects/mark-nenadov-s-bug-year-2023">data</a> I gathered.
+                In 2023, I documented 670 research-grade iNaturalist Arthropod species in Essex County, Ontario.  Check out the <a className="underline text-blue-900" href="https://www.cbc.ca/listen/live-radio/1-80-afternoon-drive/clip/15991630-a-bug-year">CBC radio interview</a> and the <a className="underline text-blue-900" href="https://inaturalist.ca/projects/mark-nenadov-s-bug-year-2023">data</a> I gathered.
               </p>
 
               <GenericButton 
