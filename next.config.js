c8246b05d05c8f120975dec97c370742157e0a12
Mark Nenadov <marknenadov@gmail.com>
2024-12-21 09:20:27 -0500

    add some more security headers

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
                 key: 'Strict-Transport-Security',
                 value: 'max-age=63072000; includeSubDomains; preload',
               },
+              {
+                key: 'X-Permitted-Cross-Domain-Policies',
+                value: 'none',
+              },
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+              },
+              {
+                key: 'Cross-Origin-Opener-Policy',
+                value: 'same-origin',
+              },
             ],
           },
         ]
