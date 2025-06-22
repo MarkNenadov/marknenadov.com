1d66e095601971554e0f8cb5c8595313ca337b4e
Mark Nenadov <marknenadov@gmail.com>
2025-06-21 21:01:48 -0400

    add credit to cursor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # marknenadov.com
 
-This site was built with React, Next.js, Typescript, Tailwind, VS Code, and Vercel
+This site was built with React, Next.js, Typescript, Tailwind, VS Code, Cursor, and Vercel
 
 Because why shouldn't I use Next.js to build my absurdly simple personal site? 😂 
 
--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <div className="w-full border border-1 border-black border-opacity-20 p-2 text-center">
-        🚀🚀 This site was built with React, Next.js, Typescript, Tailwind, Bun, VS Code, and Vercel. 🚀🚀 
+        🚀🚀 This site was built with React, Next.js, Typescript, Tailwind, Bun, VS Code, Cursor, and Vercel. 🚀🚀 
         <a 
           className="ml-2 underline text-blue-900" 
           href="https://github.com/MarkNenadov/marknenadov.com">
