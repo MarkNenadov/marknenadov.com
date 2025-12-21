391e294a2f188369f8998ee9d2f8db56c7827e97
Mark Nenadov <marknenadov@gmail.com>
2025-12-20 19:37:39 -0500

    fleshed out photography seciton a bit more.

--- app/Components/BottomRightCard.tsx
+++ app/Components/BottomRightCard.tsx
@@ -8,11 +8,14 @@ export const BottomRightCard = () => {
             I&apos;m an avid reader and you can find me on <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://www.goodreads.com/user/show/4015564-mark-nenadov">Goodreads</a>. I&apos;ve also collected <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://quotes.marknenadov.com">quotes from my reading</a>.
           </p>
           <div className="font-semibold text-base text-text-primary mb-2">
-          📷 Photography
+          📷 Wildlife Photography
           </div>
 
           <p className="text-text-secondary leading-relaxed text-sm">
-            My wildlife photography has been used by <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://www.smithsonianmag.com/smart-news/can-snakes-recognize-themselves-one-species-has-passed-a-modified-mirror-test-based-on-smell-180984088/">Smithsonian Magazine</a>, <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://ontarionature.org/flying-northwards-again-blog/">Ontario Nature</a>, <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://dnr.maryland.gov/education/Documents/MD-Herps-2021-Envirothon.pdf">Maryland DNR</a>, and <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://www.fishwildlife.org/application/files/3317/1052/2834/2023-AFWA-AnnualReport-v01F.pdf">Association of Fish & Wildlife Agencies</a>.
+            My photography has appeared in <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://www.smithsonianmag.com/smart-news/can-snakes-recognize-themselves-one-species-has-passed-a-modified-mirror-test-based-on-smell-180984088/">Smithsonian Magazine</a>, <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://ontarionature.org/flying-northwards-again-blog/">Ontario Nature</a>, <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://dnr.maryland.gov/education/Documents/MD-Herps-2021-Envirothon.pdf">Maryland DNR</a>, <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://www.fishwildlife.org/application/files/3317/1052/2834/2023-AFWA-AnnualReport-v01F.pdf">Association of Fish & Wildlife Agencies</a>.
+          </p>
+          <p className="text-text-secondary leading-relaxed text-sm">
+            My photography is on <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://www.instagram.com/MarkNenadov">Instagram</a> and <a className="underline text-primary-700 hover:text-primary-600 transition-colors font-medium" href="https://www.flickr.com/photos/marknenadov/">Flickr</a>.
           </p>
         </Card.Content>
       </Card>
